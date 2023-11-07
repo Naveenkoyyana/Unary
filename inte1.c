ab7f1df0494b0bae239d6bed01e5bf6051a45b17
Naveenkoyyana <naveenkoyyana68@gmail.com>
2023-11-07 12:15:15 +0530

    second commit

---
+++ inte1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    int a = 8;
+    int b = 9;
+    int c = a+b;
+    printf("a+b %d/n", c);
+    return 0;
+}
---
+++ uanary1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter value a");
+    scanf("%d", &a);
+    int b=-a;
+    printf("%d", b=-a);
+    return 0;
+}
