1d98921bcbe31a7862ef40228b688a53d1eb3803
Naveenkoyyana <naveenkoyyana68@gmail.com>
2023-11-27 13:40:29 +0530

    lk

---
+++ array13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+        int a[7],b[9],sum (i,j);
+        printf("enter the values:\n");
+        for(i=0; i<7; i++)
+        
+        {
+            scanf("%d", &a[i]);
+        }
+
+        printf("enter the array:\n");
+        for(j=0; j<9; j++)
+        {
+            scanf("%d", &b[j]);
+        }
+
+        printf("display the elements:\n");
+        for(i=0; i<7; i++)
+        for(j=0; j<9; j++)
+        {
+        scanf("%d",a[i]);
+        scanf("%d",b[j]);
+        }
+        return 0;
+}            
+
+        
+
+
+
+
+
+
---
+++ name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("hi Naveen this is my first programe");
+    return 0;
+}
